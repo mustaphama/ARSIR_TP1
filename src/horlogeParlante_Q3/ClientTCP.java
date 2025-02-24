106b67ffc4a94347afb15c8cf8db4b33980f7623
CHABCHOUB SALIM p2313362 <salim.chabchoub@etu.univ-lyon1.fr>
2025-02-24 23:25:42 +0100

    question 3

--- src/horlogeParlante_Q3/ClientTCP.java
+++ src/horlogeParlante_Q3/ClientTCP.java
@@ -1,3 +1,4 @@
+package horlogeParlante_Q3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- src/horlogeParlante_Q3/ServerTCP.java
+++ src/horlogeParlante_Q3/ServerTCP.java
@@ -1,3 +1,4 @@
+package horlogeParlante_Q3;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
