70f63c129bc39c9f6fc08deec0fe534b1004edd1
saliml02 <145446470+saliml02@users.noreply.github.com>
2025-02-25 17:06:01 +0100

    Update Client.java

--- src/horlogeParlante_Q2/Client.java
+++ src/horlogeParlante_Q2/Client.java
@@ -28,34 +28,23 @@ public class Client {
             // Heure de réception T₂
             long T2 = System.currentTimeMillis();
 
-            // Extraire T₁, T'₁ et T'₂ du message reçu
+            // Extraire T'₁ reçu du serveur
             String receivedData = new String(response.getData(), 0, response.getLength());
-            String[] timestamps = receivedData.split(",");
-            long T1Received = Long.parseLong(timestamps[0]);
-            long T1Prime = Long.parseLong(timestamps[1]);
-            long T2Prime = Long.parseLong(timestamps[2]);
+            long T1Prime = Long.parseLong(receivedData);  // ✅ Conversion sûre car le serveur envoie un `long`
 
             // Calcul du délai de transmission δ
-            long delta = (T2 - T1) - (T2Prime - T1Prime);
+            long delta = (T2 - T1) - (T1Prime - T1);
 
             // Calcul du décalage des horloges θ
-            long theta = ((T1Prime + T2Prime) / 2) - ((T1 + T2) / 2);
+            long theta = (T1Prime - T1);
 
-            System.out.println("Réponse reçue : T1 = " + new SimpleDateFormat("HH:mm:ss:SSS").format(new Date(T1Received)) + ", T'1 = " + new SimpleDateFormat("HH:mm:ss:SSS").format(new Date(T1Prime))  + ", T'2 = " + new SimpleDateFormat("HH:mm:ss:SSS").format(new Date(T2Prime)) );
-            System.out.println("T2 = " + T2);
+            System.out.println("Réponse reçue : T'1 = " + new SimpleDateFormat("HH:mm:ss:SSS").format(new Date(T1Prime)));
+            System.out.println("T2 = " + new SimpleDateFormat("HH:mm:ss:SSS").format(new Date(T2)));
             System.out.println("Délai de transmission  = " + delta + " ms");
             System.out.println("Décalage des horloges  = " + theta + " ms");
-
-            // Ajuster l'horloge du client si besoin
-            System.out.println("Nouvelle horloge client = " + ( new SimpleDateFormat("HH:mm:ss:SSS").format(new Date(T1Received)) + ", T'1 = " + new SimpleDateFormat("HH:mm:ss:SSS").format(new Date(T1Prime))  + ", T'2 = " + new SimpleDateFormat("HH:mm:ss:SSS").format(new Date(T2 + theta))) + " ms");
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-
-
-
-// UDP est utilisé car il est plus rapide que TCP, mais il n'offre pas de garantie de livraison des paquets.
